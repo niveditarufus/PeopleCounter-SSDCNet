90b6000152632cf1fec4faf33061f72ddd27ec21
niveditarufus <nive.hardy@gmail.com>
2020-06-11 21:52:52 +0530

    clean up

--- load_data_V2.py
+++ load_data_V2.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-author: xionghaipeng
-"""
 
-__author__='xhp'
-
-'''load the dataset'''
-#from __future__ import print_function, division
 import os
 import torch
 import numpy as np
