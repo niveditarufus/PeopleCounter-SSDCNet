f4a673ef896b621f5099ec6bcc7fc097fab6fde1
niveditarufus <nive.hardy@gmail.com>
2020-06-15 23:01:06 +0530

    minor changes

--- main_process.py
+++ main_process.py
@@ -17,34 +17,6 @@ from Val import test_phase
 import queue, threading
 
 
-# # bufferless VideoCapture
-# class VideoCapture:
-
-#   def __init__(self, name):
-#     self.cap = cv2.VideoCapture(name)
-#     self.q = queue.Queue()
-#     t = threading.Thread(target=self._reader)
-#     t.daemon = True
-#     t.start()
-
-#   # read frames as soon as they are available, keeping only most recent one
-#   def _reader(self):
-#     while True:
-#       ret, frame = self.cap.read()
-#       if not ret:
-#         break
-#       if not self.q.empty():
-#         try:
-#           self.q.get_nowait()   # discard previous (unprocessed) frame
-#         except Queue.Empty:
-#           pass
-#       self.q.put(frame)
-
-#   def read(self):
-#     return self.q.get()
-
-
-
 def test(frame, opt, rgb, transform_test, num_workers, label_indice, model_path):
     img = Image.fromarray(frame)
     testset = Countmap_Dataset(img,rgb,transform=transform_test,\
